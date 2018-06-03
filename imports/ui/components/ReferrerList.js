58d6757a5ce84e995b380eb59680dd10d8e24e67
Sylvain Duchesne <pro@sylvainduchesne.com>
2018-06-03 16:36:26 +0200

    automatic scrolling centered on user's position in list

--- imports/ui/components/ReferrerList.js
+++ imports/ui/components/ReferrerList.js
@@ -1,32 +1,49 @@
 import React from 'react'
 import {Table} from 'reactstrap'
 
-export default ({list, referrer}) => {
-  return (
-    <Table striped responsive>
-      <thead>
-        <tr>
-          <th>Rank</th>
-          <th>Username</th>
-          <th>Parrainés</th>
-          <th>Points</th>
-        </tr>
-      </thead>
-      <tbody>
-        {list.map(({userId, username, rank, referralCount}) => (
-          <tr key={userId}>
-            <td>{rank}</td>
-            <td>
-              {userId === referrer.userId &&
-                <b>{username || userId}</b> ||
-                username || userId
-              }
-            </td>
-            <td>{referralCount}</td>
-            <td>{referralCount}</td>
+export default class ReferrerList extends React.Component {
+  componentDidUpdate(prevProps) {
+    const {referrer} = prevProps
+    document.getElementById(referrer.userId)
+    .scrollIntoView({block: 'center'})
+  }
+  render() {
+    const {list, referrer} = this.props
+    const tbodyStyle = {
+      display:   'block',
+      maxHeight: '300px',
+      overflowY: 'scroll',
+      width:     '100%',
+    }
+    const trStyle = {
+      display:     'table',
+      width:       '100%',
+      tableLayout: 'fixed',
+    }
+    return (
+      <Table striped responsive>
+        <thead>
+          <tr style={trStyle}>
+            <th>Rank</th>
+            <th>Username</th>
+            <th>Points</th>
           </tr>
-        ))}
-      </tbody>
-    </Table>
-  )
+        </thead>
+        <tbody style={tbodyStyle}>
+          {list.map(({userId, username, rank, referralCount}) => (
+            <tr key={userId} style={trStyle} id={userId}>
+              <td>{rank}</td>
+              <td>
+                {userId === referrer.userId &&
+                  <b>{username || userId}</b> ||
+                  username || userId
+                }
+              </td>
+              <td>{referralCount}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    )
+  }
 }
