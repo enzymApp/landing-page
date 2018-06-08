c231f9d4cf89d6f08cc67c043df9bc91d3f34e3d
Ethnolatin <fduchesne@ethnolatin.com>
2018-06-08 08:51:32 +0200

    wip: twitter link

---
+++ imports/ui/components/TwitterLink.js
@@ -0,0 +1,28 @@
+
+import React from 'react'
+
+export default () => {
+  return (
+    <button onClick={twitterLink}>
+      Twitter link
+    </button>
+  )
+}
+
+twitterLink = () => (
+    <a href="http://ethnolatin.com" target="_blank"></a>
+)
+
+
+
+// facebook share button
+
+
+
+
+// facebook like button
+
+
+
+
+// google+ like button
