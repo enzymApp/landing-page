67cb24387eed7db534482f6de5fc32054327f9c5
Sylvain Duchesne <pro@sylvainduchesne.com>
2018-06-19 11:15:47 +0200

    fix social login email bug

--- imports/api/users/Users.js
+++ imports/api/users/Users.js
@@ -2,11 +2,11 @@ import {Meteor} from 'meteor/meteor'
 
 Meteor.users.helpers({
   email() {
-    console.log("user.email()", this)
+    console.log("user.email()", JSON.stringify(this))
     if(this.registered_emails)                return this.registered_emails[0]
     if(this.emails && this.emails.length > 0) return this.emails[0]
-    if(this.services.facebook)                return this.services.facebook.email
-    if(this.services.google)                  return this.services.google.email
-    if(this.services.twitter)                 return this.services.twitter.email
+    if(this.services.facebook)                return {address: this.services.facebook.email}
+    if(this.services.google)                  return {address: this.services.google.email}
+    if(this.services.twitter)                 return {address: this.services.twitter.email}
   }
 })
