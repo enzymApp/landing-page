0b280985e7a5ca886a6bc7604ad1af5a5db44c95
Sylvain Duchesne <pro@sylvainduchesne.com>
2018-06-19 15:46:51 +0200

    fix user.emails front bug

--- imports/api/users/Users.js
+++ imports/api/users/Users.js
@@ -2,9 +2,9 @@ import {Meteor} from 'meteor/meteor'
 
 Meteor.users.helpers({
   email() {
-    console.log("user.email()", JSON.stringify(this))
     if(this.registered_emails)                return this.registered_emails[0]
     if(this.emails && this.emails.length > 0) return this.emails[0]
+    if(!this.services) return
     if(this.services.facebook)                return {address: this.services.facebook.email}
     if(this.services.google)                  return {address: this.services.google.email}
     if(this.services.twitter)                 return {address: this.services.twitter.email}
