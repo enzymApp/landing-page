e6dc75a2ae4cab06b369770e9c08f0e28497b540
Ethnolatin <fduchesne@ethnolatin.com>
2018-10-04 17:03:43 +0200

    fix destructuring issue in prototypeDdpConnection

--- imports/startup/server/prototypeDdpConnection.js
+++ imports/startup/server/prototypeDdpConnection.js
@@ -1,7 +1,9 @@
 import {Accounts} from 'meteor/accounts-base'
 import {Meteor}   from 'meteor/meteor'
 
-export default ({username, password}) => {
+export default (settings) => {
+  if(!settings) return
+  const {username, password} = settings
   if(!Meteor.users.findOne({username})) {
     console.log(`create account for ${username}`)
     Accounts.createUser({username, password, profile: {technical: true}})
