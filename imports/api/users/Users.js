6edc1b5a4abce34454f99b1398f5ca42128d06a7
Sylvain Duchesne <pro@sylvainduchesne.com>
2018-10-24 18:16:58 +0200

    update user schema

--- imports/api/users/Users.js
+++ imports/api/users/Users.js
@@ -1,4 +1,60 @@
-import {Meteor} from 'meteor/meteor'
+import {Meteor}     from 'meteor/meteor'
+import SimpleSchema from 'simpl-schema'
+
+const UserProfile = new SimpleSchema({
+  city:          { type: String,     optional: true },
+  contest:       { type: Boolean,    optional: true },
+  country:       { type: String,     optional: true },
+  geoloc:        { type: String,     optional: true },
+  lang :         { type: String,     optional: true },
+  referrerToken: { type: String,     optional: true },
+  region:        { type: String,     optional: true },
+  technical:     { type: Boolean,    optional: true },
+})
+
+Meteor.users.schema = new SimpleSchema({
+  username:          { type: String, optional: true },
+  blockedUsers:      {type: Array,   defaultValue: []},
+  emails:            { type: Array,  optional: true },
+  "emails.$": { type: Object },
+  "emails.$.address": { type: String, regEx: SimpleSchema.RegEx.Email },
+  "emails.$.verified": { type: Boolean },
+  registered_emails: { type: Array, optional: true },
+  'registered_emails.$': { type: Object, blackbox: true },
+  createdAt:         { type: Date },
+  profile:           { type: UserProfile, optional: true,  defaultValue: {} },
+  services:          { type: Object,      optional: true,  blackbox: true },
+  zyms:              { type: Number,      defaultValue: 0, min: 0 },
+  roles:             { type: Array,       optional: true},
+  'roles.$':         { type: String},
+  authMean: {
+    type: String,
+    autoValue() {
+      this.unset()
+      if(!this.field('services').isSet) return
+      const services = this.field('services').value
+      if(!services)         return 'email'
+      if(services.facebook) return 'facebook'
+      if(services.google)   return 'google'
+      if(services.twitter)  return 'twitter'
+      return 'email'
+    },
+  },
+  heartbeat: { type: Date, optional: true }
+})
+
+Meteor.users.attachSchema(Meteor.users.schema)
+
+
+Meteor.users.getFromEmail = (address, options) => Meteor.users.find({
+  $or: [
+    {registered_emails: {$elemMatch: {address}}},
+    {emails: {$elemMatch: {address}}},
+    {'services.facebook.email': address},
+    {'services.google.email': address},
+    {'services.twitter.email': address},
+  ]
+}, options)
 
 Meteor.users.helpers({
   email() {
